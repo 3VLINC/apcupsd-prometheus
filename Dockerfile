da92fff97cde9d1d0109ccc62b19c513d0dba617
Dan Imbrogno <dan@3vl.ca>
2023-05-23 15:08:18 -0400

    Remove superfluous volumes, add envs

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
 FROM --platform=amd64 node:18-bullseye-slim 
 ENV APCUPSD_OUTPUT_FILEPATH=apcupsd.prom
 ENV APCUPSD_DEBUG=
+ENV APCUPSD_POLL_CRON=
+ENV APCUPSD_TIMEOUT=
 RUN npm install pm2@5.3.0 -g
 RUN apt-get update \
     && apt-get -y install --no-install-recommends \
@@ -12,5 +14,4 @@ COPY ./src /root/
 WORKDIR /root
 RUN npm ci
 VOLUME /root/textfile_collector
-VOLUME /etc/apcups/apcupsd.conf
 CMD ["/root/entrypoint.sh"]
--- src/main.js
+++ src/main.js
@@ -7,6 +7,8 @@ const outputFolder = resolve(__dirname, "./textfile_collector");
 const outputFilename = process.env.APCUPSD_OUTPUT_FILEPATH;
 const outputPath = resolve(outputFolder, outputFilename);
 const debug = process.env.APCUPSD_DEBUG ? true : false;
+const schedule = process.env.APCUPSD_POLL_CRON ? process.env.APCUPSD_POLL_CRON : "* * * * *";
+const timeout = process.env.APCUPSD_TIMEOUT ? process.env.APCUPSD_TIMEOUT : 30000;
 const parseData = require("./parseData");
 const cron = require("node-cron");
 const { exec } = require('child_process');
@@ -18,21 +20,28 @@ const log = (...msg) => {
 };
 
 const main = async () => {
+
+  if (!cron.validate(schedule)) {
+    log(`Invalid cron schedule: ${schedule}`);
+    process.exit(1);
+  }
+
   log(`Starting
     Output Path: ${outputPath}
     Socket Path: ${JSON.stringify(socketPath)}
+    Cron Schedule: ${schedule}
 
 Waiting for socket...
   `);
   await waitOn({
     resources: [`tcp:${socketPath.port}`],
-    timeout: 30000, // fail if waiting longer than 30 seconds
+    timeout, // fail if waiting longer than 30 seconds
   })
     .then(() => log("Creating output folder..."))
     .then(() => fs.mkdir(outputFolder, { recursive: true }))
     .then(async () => {
       log("Connecting...");
-      return cron.schedule("* * * * *", async () => {
+      return cron.schedule(schedule, async () => {
         log("Getting status...");
         try {
           // TODO: safer way to do this?
